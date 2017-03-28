9a5b5c36989f7c2d0fea503cc4eda8ee1b791656
Heber Holanda <heberpcl@gmail.com>
2017-03-28 15:11:17 -0300

    getInterfaceReceivedBytes()

--- services/interfaceFunctions.php
+++ services/interfaceFunctions.php
@@ -5,3 +5,14 @@
         return $uname[0] === OS_LINUX;
     }
     
+    function getInterfaceReceivedBytes($interface)
+    {
+        if (isLinux()) {
+            $filepath = '/sys/class/net/%s/statistics/rx_bytes';
+            $output = file_get_contents(sprintf($filepath, $interface));
+
+            return $output;
+        }
+        throw new Exception('Unable to guess OS');
+    }
+    
